a0abaf00e9e051c0aa601a1775dd1efde89629b0
Abbas Taghilooei <abbastaghilooei@gmail.com>
2021-07-03 11:35:08 +0430

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', '290083');
-define('DB_PASSWORD', 'scd874332PL');
-define('DB_NAME', '290083');
-
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'pass');
+define('DB_NAME', 'dbname');
+// user pass and db name should set by the user and changed for uploading here
 $dataLink = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 if($dataLink === false){
